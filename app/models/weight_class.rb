499669d4a1928cf2e7363dd3d2d0df1c753e5e4e
AP-Lett <audreana.lett@gmail.com>
2025-02-21 20:33:04 +1000

    Added validations on weight class model

--- app/models/weight_class.rb
+++ app/models/weight_class.rb
@@ -1,4 +1,12 @@
 class WeightClass < ApplicationRecord
   has_many :wrestlers, dependent: :destroy
 
+  validates :wc, 
+    presence: true,
+    numericality: { only_integer: true},
+    inclusion: {
+      in: 97..285,
+      message: 'must be between 97 and 285 pounds'
+    }
+
 end

cf8d48a46f78717ccebd7bac26bad81489203f9b
AP-Lett <audreana.lett@gmail.com>
2025-02-21 20:52:47 +1000

    fixed wrestler update issue

--- app/controllers/wrestlers_controller.rb
+++ app/controllers/wrestlers_controller.rb
@@ -16,13 +16,13 @@ class WrestlersController < ApplicationController
 
   def update #works
     wrestler = Wrestler.find(params[:id])
-    wrestler.update!(name: params[:name], age: params[:age], gender: params[:gender], weight_class_id: params[:weight_class_id])
+    wrestler.update(name: params[:name] || wrestler.name, age: params[:age] || wrestler.age, gender: params[:gender]|| wrestler.gender, weight_class_id: params[:weight_class_id] || wrestler.weight_class_id)
     render json: wrestler
   end
 
   def destroy #works
     wrestler = Wrestler.find(params[:id])
-    wrestler.destroy!
+    wrestler.destroy
     render :no_content
   end
 end

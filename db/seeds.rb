a3208a552a10adfc230134f1b1e6c11c0a05485b
AP-Lett <audreana.lett@gmail.com>
2025-02-24 18:42:19 +1000

    WK6D1: seeds, faker init

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+WeightClass.create!(wc: 97)
+WeightClass.create!(wc: 132)
+WeightClass.create(wc: 155)
+
+10.times do 
+  Wrestler.create!(
+    name: Faker::Name.name, 
+    age: 15,
+    gender: "Female",
+    weight_class_id: 2
+  )
+end
+
+puts "Created 3 Weight classes, and adds 10 wrestlers"
+
